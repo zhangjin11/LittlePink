50cd364280aa017a58b4d908e00bf764905d52af
john <873023467@qq.com>
2022-05-20 08:54:30 +0800

    Initial Commit

---
+++ LittlePink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink
+//
+//  Created by john on 2022/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
